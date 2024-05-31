00dbd71d328e4d0aa4f093142ed977713094e53d
Valeria Sidnenko <46744884+valsidnenko@users.noreply.github.com>
2024-05-31 15:27:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="formularity_rfs",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "spacy",
